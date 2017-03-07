29a8983eeea6d217e7d26dbc5383ebc7017069d4
sawra <sawra@uw.edu>
2017-03-06 16:56:08 -0800

    added morgan, etc

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 let express = require('express');
+let morgan = require('morgan');
+let logger = morgan("tiny");
 
 let app = express();
+
+app.use(logger);
+app.use(function (req, res, next) {
+    // do your logging here
+    console.log(req.method, req.url);
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    next();
+});
+
+app.use('/pupz', function(req,res,next){
+    res.send("<p>pupz func</p>");
+})
+
+app.listen(3000);
