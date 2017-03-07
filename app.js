7d0c0d760c8497ba9944c8ec177fd0456ba03eb8
Andrew Hookom <hookom@gmail.com>
2017-03-06 20:25:42 -0500

    setup nunjucks

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 let express = require('express');
 let morgan = require('morgan');
-let logger = morgan("tiny");
+let nunjucks = require('nunjucks');
+let fs = require('fs');
+
+let logger = morgan('tiny');
 
 let app = express();
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+//nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+nunjucks.configure('views', { noCache: true });
 
 app.use(logger);
 app.use(function (req, res, next) {
@@ -12,8 +20,32 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use('/pupz', function(req,res,next){
-    res.send("<p>pupz func</p>");
+app.use('/pupz', function(req, res, next){
+    res.send("pupzzzz");
+})
+
+app.get('/', function(req, res, next){
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
+
+
 })
 
 app.listen(3000);
+
+
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'},
+//         { name: 'Grima'}
+//     ]
+// };
+// nunjucks.configure('views', {noCache: true});
+
+// nunjucks.render('index.html', locals, function (err, output) {
+//   if (err) throw err;
+//   console.log(output);
+// });
